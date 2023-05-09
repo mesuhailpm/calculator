1b800d6fb9d31d2e109e51800b768d9b8bed51e2
mesuhailpm <tosuhailpm@gmail.com>
2023-05-09 16:22:42 +0530

    Updates

--- src/components/OperatorComponent/OperatorComponent.js
+++
@@ -1,8 +0,0 @@
-import { CHOOOSE_OPERATOR } from "../../constants/constants";
-
-export default function DigitComponent({operator,dispatch}){
-    return(
-        <button onClick={()=>dispatch({type:CHOOOSE_OPERATOR,payload:operator})}>{operator}</button>
-
-    )
-}
--- src/constants/constants.js
+++
@@ -1,7 +0,0 @@
-const ADD_DIGIT = 'add digit'
-const CHOOOSE_OPERATOR = 'choose operator'
-const EVALUATE = 'evaluate'
-const DELETE = 'delete'
-const CLEAR = 'clear'
-
-export {ADD_DIGIT,CHOOOSE_OPERATOR,EVALUATE,DELETE,CLEAR}
